43563abf1b9cb828b33f3ed21c07eaec25a1ad30
RW_AI <103892538+RWAI524@users.noreply.github.com>
2022-11-03 10:30:14 -0400

    commit

--- Template Project/Assets/Scripts/ExampleScript.cs
+++ Template Project/Assets/Scripts/ExampleScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ExampleScript : MonoBehaviour
 {
     public int var1;
+    public int var2;
 
     // Start is called before the first frame update
     void Start()
